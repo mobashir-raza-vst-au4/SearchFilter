f2ab66881cacdf29f84fc7cfc6bba14a83bd817a
mobashir-raza-vst-au4 <54476228+mobashir-raza-vst-au4@users.noreply.github.com>
2020-07-10 01:23:02 +0530

    changes in nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -32,15 +32,25 @@ export default connect(getData, getFunction)(class Header extends Component {
         return (
             <div>
                 <nav className="navbar navbar-dark bg-dark fixed-top">
-                    <div className="container-fluid justify-content-center">
-                        <div className="d-flex">
-                        <input onChange={(e) => {this.handleInput(e)}} className="form-control mx-2" type="search" placeholder="Search" aria-label="Search" />
-                    </div>
-                    </div>
-                </nav>
+                        
+                        <div className="container-fluid">
+                        <div className = "row ">
+                        <div className = "col-md-4">
+                        <h1 className="navbar-brand" >Gongo</h1>
+                        </div>
+                        <form className="col-lg-4 d-flex justify-content-center ">
+                        <input onChange={(e) => {this.handleInput(e)}} className="form-control mx-5" type="search" placeholder="Search" aria-label="Search" />
+                        </form>     
+                        </div>
+                        </div>
+                        
+                        </nav>
             </div>
         )
     }
 })
+
+
+
 
 
