f7e34f9df87d77ec8e7c0a44ae365548c2b8ca7f
mobashir-raza-vst-au4 <54476228+mobashir-raza-vst-au4@users.noreply.github.com>
2020-07-09 22:55:33 +0530

    deploy

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 let getData = (store) => {
-    console.log(store)
+    // console.log(store)
     return {
         store
     }
